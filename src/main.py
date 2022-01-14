fc4a9d94ac208875d8f9f97f5664f72ead090828
kim01414 <kim01414@knu.ac.kr>
2022-01-14 12:00:02 +0900

    코드 생성

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import tkinter as TK
+
+class MAIN_PROGRAM:
+    def __init__(self):
+        self.WINDOW = TK.Tk()
+        self.WINDOW.mainloop()
+
+if __name__ == '__main__':
+    MAIN = MAIN_PROGRAM()
